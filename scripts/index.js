7b3db6019c093f3be9cbbf5c4f0d6d5fbc0b49e4
Karina Valiaeva <karinaval1989@yandex.ru>
2020-12-04 12:17:31 +0300

    array

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__button-close');
@@ -6,6 +33,7 @@ let profileJob = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
+let cards = document.querySelector('.cards');
 
 function addPopupOpened() {
     popup.classList.add('popup_opened');
@@ -27,4 +55,3 @@ function formSubmitHandler(evt) {
 profileEditButton.addEventListener('click', addPopupOpened);
 popupCloseButton.addEventListener('click', removePopupOpened);
 formElement.addEventListener('submit', formSubmitHandler);
-
