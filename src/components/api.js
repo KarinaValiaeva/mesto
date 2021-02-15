60959f3ab19a595cab9ac994e06471aeb3427986
Karina Valiaeva <karinaval1989@yandex.ru>
2021-02-15 14:28:54 +0300

    render cards and create new card

--- src/components/api.js
+++ src/components/api.js
@@ -1,18 +1,70 @@
-class Api {
-    constructor(data) {
-      this._baseUrl = data.baseUrl;
-      this._headers = data.headers
-    }
-  
-    getInitialCards() {
-        return fetch(this._baseUrl, this._headers)
-          .then(res => {
-            if (res.ok) {
-              return res.json();
-            }
-          });
-      }
-  
-    // другие методы работы с API
+export default class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
   }
+
+  getUserInfo() {
+    return fetch(this.baseUrl+'/users/me ', { headers: this.headers })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+  }
+
+  getInitialCards() {
+    return fetch(this.baseUrl+'/cards', { headers: this.headers })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+  }
+
+  postCard(data) {
+    return fetch(this.baseUrl+'/cards', {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link
+       // link: 'https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_1280.jpg'
+    })
+  })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+  }
+
+
+  patch() {
+     return fetch(this.baseUrl+'/users/me', {
+  method: 'PATCH',
+  headers: this.headers,
+  body: JSON.stringify({
+    name: 'Marie Skłodowska Curie',
+    about: 'Physicist and Chemist'
+  })
+})
+
+.then(res => {
+  if (res.ok) {
+    return res.json();
+  }
+  // если ошибка, отклоняем промис
+  return Promise.reject(`Ошибка: ${res.status}`);
+});
+  }
+
+  // другие методы работы с API
+}
   
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import './index.css';
+import Api from '../components/api';
 import {
     initialCards,
     profileEditButton,
@@ -30,26 +31,106 @@ newCardFormValidator.enableValidation();
 const popupWithImage = new PopupWithImage('.popup_image');
 popupWithImage.setEventListeners();
 
-// отрисовка карточек в секции cards
-const cardList = new Section({
-    items: initialCards,
-    renderer: (item) => {
-        const cardElement = createCard(item)
-        cardList.addItem(cardElement);
+
+const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-20',
+    headers: {
+      authorization: '3b3ba091-9f36-415d-9c11-8ad47d6b1eca',
+      'Content-Type': 'application/json'
     }
-}, '.cards');
-cardList.renderItems();
+  });
+
+//console.log(api.getUserInfo());
+// api.patch();
+
+
+
+
+// отрисовка карточек в секции cards
+
+  api.getInitialCards()
+      .then((cards) => {
+          const cardList = new Section({
+             items: cards,
+            
+              renderer: ((item) => {
+                  const cardElement = createCard(item);
+                 console.log(cardElement);
+                  cardList.addItem(cardElement);
+                  //cardElement.querySelector('.card__btn-remove').remove();
+                })
+          }, '.cards');
+          cardList.renderItems();
+      });
+
+      // создание новой карточки
+      const popupCardsWithForm = new PopupWithForm(
+        '.popup_cards',
+        {
+            handleFormSubmit: (item) => {
+                api.postCard(item)
+                .then ((item) => {
+                        const cardElement = createCard(item);
+                          
+                        const cardList = document.querySelector('.card');
+                        console.log(cardList); 
+                        cardList.prepend(cardElement);
+                    })
+                    }
+                    
+            }
+        );
+    popupCardsWithForm.setEventListeners();
+    
+
+//      const cardList = new Section({
+//     //    items: post,
+//         renderer: 
+       
+//         api.getInitialCards()
+//         .then((item) => {
+
+//              const cardElement = createCard(item);
+//              console.log(cardElement);
+//              cardList.addItem(cardElement);
+//              cardElement.querySelector('.card__btn-remove').remove();
+//          })
+
+//      }, '.cards');
+//    //  cardList.renderItems();
+
+
+
+
+// const popupCardsWithForm = new PopupWithForm(
+//     '.popup_cards',
+//     {
+//         handleFormSubmit: (item) => {
+//             api.postCard(item)
+//             .then ((item) => {
+//                     const cardElement = createCard(item);
+//                     console.log(cardElement);
+//                     cardList.prependItem(cardElement);
+                   
+//                 })
+//                 }
+                
+//         }
+//     );
+// popupCardsWithForm.setEventListeners();
+
 
 // создание экземпляра класса для попапа добавления новой карточки
-const popupCardsWithForm = new PopupWithForm(
-    '.popup_cards',
-    {
-        handleFormSubmit: (item) => {
-            const cardElement = createCard(item)
-            cardList.prependItem(cardElement);
-        }
-    });
-popupCardsWithForm.setEventListeners();
+// const popupCardsWithForm = new PopupWithForm(
+//     '.popup_cards',
+//     {
+//         handleFormSubmit: (item) => {
+//             const cardElement = createCard(item)
+//             cardList.prependItem(cardElement);
+            
+//         }
+//     });
+// popupCardsWithForm.setEventListeners();
 
 // создание экземпляра класса для отображения информации о пользователе
 const userInfo = new UserInfo({ name: '.profile__name', job: '.profile__job' });
@@ -72,6 +153,7 @@ function createCard(item) {
     return cardElements;
 }
 
+
 // нажатие на кнопку добавления новой карточки
 profileAddButton.addEventListener('click', function () {
     popupCardsWithForm.open();
@@ -84,4 +166,4 @@ profileEditButton.addEventListener('click', function () {
     nameInput.value = userInfo.getUserInfo().name;
     jobInput.value = userInfo.getUserInfo().job;
     profileFormValidator.resetValidation();
-});
+})
